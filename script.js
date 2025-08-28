987b1d77d9bf48eddcf952ad72bd1e078e6f942b
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-08-28 13:58:10 -0300

    adição do timer parte visual

--- script.js
+++ script.js
@@ -7,20 +7,53 @@ let intervalo = null
 let intensidade = document.querySelector('#intensidade_id');
 let intense = Number(intensidade.value);
 
+let hr_span = document.querySelector('#hora');
+let min_span = document.querySelector('#minutos');
+let seg_span = document.querySelector('#segundos');
+
+let hr = 0
+let min = 0
+let seg = 0
+
 let tempo = 0
 function iniciar_timer() {
     intervalo = setInterval(() => {
         if (tempo === 100) {
             clearInterval(intervalo);
-            intervalo(null);
+            intervalo = null;
             window.alert('Escolha a intensidade e salve o progresso!')
         } else {
             tempo++
         }
         barra1.style.backgroundSize = `${tempo}%, 100%`;
         n_barra1.textContent = tempo + "%";
-        
-    }, 5);
+        if (seg <= 59) {
+            if (seg < 9) {
+                seg++;
+                seg_span.textContent = `0${seg}`;
+            } else {
+                seg++;
+                seg_span.textContent = seg;
+            }
+        } else if (min <= 59) {
+            if (min < 9) {
+                min++;
+                min_span.textContent = `0${min}`;
+            } else {
+                min++;
+                min_span.textContent = min;
+            }
+            seg = 0;
+        } else {
+            hr++
+            hr_span.textContent = hr
+            min = 0;
+            seg = 0;
+            seg_span.textContent = "00";
+            min_span.textContent = "00";
+        }
+
+    }, 100);
 }
 
 function parar_timer() {
@@ -29,21 +62,21 @@ function parar_timer() {
 
 let controle_barra2 = 0
 function salvar() {
-  let intensidade = document.querySelector('#intensidade_id');
-  let intense = Number(intensidade.value);
-  let tempo_dividido = parseInt(tempo / 5);
+    let intensidade = document.querySelector('#intensidade_id');
+    let intense = Number(intensidade.value);
+    let tempo_dividido = parseInt(tempo / 5);
 
-  // Atualiza controle_barra2 acumulando progresso
-  controle_barra2 += tempo_dividido + intense;
+    // Atualiza controle_barra2 acumulando progresso
+    controle_barra2 += tempo_dividido + intense;
 
-  // Atualiza visual da barra total
-  barra2.style.backgroundSize = `${controle_barra2}% 100%`;
-  n_barra2.textContent = `${controle_barra2}%`;
+    // Atualiza visual da barra total
+    barra2.style.backgroundSize = `${controle_barra2}% 100%`;
+    n_barra2.textContent = `${controle_barra2}%`;
 
-  // Reseta barra1
-  tempo = 0;
-  barra1.style.backgroundSize = `0% 100%`;
-  n_barra1.textContent = "0%";
+    // Reseta barra1
+    tempo = 0;
+    barra1.style.backgroundSize = `0% 100%`;
+    n_barra1.textContent = "0%";
 
-  console.log("Progresso acumulado:", controle_barra2);
+    console.log("Progresso acumulado:", controle_barra2);
 }
