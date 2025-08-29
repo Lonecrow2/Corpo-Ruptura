621b20b704f30a6ff2f563451b918e0a7eb20dd0
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-08-29 10:50:18 -0300

    Copia da logica inicial
    Fiz uma copia da minha logica inicial, pois vou te que mudar o timer pelo falto de nao funcionar com pagina inativa

---
+++ Copia_minha_logica_inicial/script.js
@@ -0,0 +1,71 @@
+let barra1 = document.querySelector('#barra_tempo_treino');
+let barra2 = document.querySelector('#barra_tempo_treino_total');
+let n_barra1 = document.querySelector('#barra_tempo_treino_n');
+let n_barra2 = document.querySelector('#barra_tempo_treino_n_total');
+let start = document.querySelector('#iniciar');
+let intervalo = null
+let intensidade = document.querySelector('#intensidade_id');
+let intense = Number(intensidade.value);
+
+let hr_span = document.querySelector('#hora');
+let min_span = document.querySelector('#minutos');
+let seg_span = document.querySelector('#segundos');
+
+let hr = 0, min = 0, seg = 0;
+
+
+let tempo = 0
+function iniciar_timer() {
+    intervalo = setInterval(() => {
+        tempo++
+        let tempo_porcentagem = parseInt((tempo / 3600) * 100);
+        if (tempo === 3600) {
+            clearInterval(intervalo);
+            intervalo = null;
+            setTimeout(() => {
+                window.alert('Escolha a intensidade e salve o progresso!');
+            }, 1000);
+        }
+        barra1.style.backgroundSize = `${tempo_porcentagem}%, 100%`;
+        n_barra1.textContent = tempo_porcentagem + "%";
+        if (seg < 59) {
+            seg++;
+            seg_span.textContent = seg < 10 ? `0${seg}` : seg;
+        } else {
+            seg = 0;
+            seg_span.textContent = "00";
+            if (min < 59) {
+                min++;
+                min_span.textContent = min < 10 ? `0${min}` : min;
+            } else {
+                min = 0;
+                min_span.textContent = "00";
+                hr++;
+                hr_span.textContent = hr;
+            }
+        }
+    }, 1);
+}
+
+function parar_timer() {
+    clearInterval(intervalo);
+}
+
+let soma_progresso = 0
+function salvar() {
+    hr = 0, min = 0, seg = 0;
+    let tempo_porcentagem_progresso = parseInt((tempo / 18000) * 100);
+    soma_progresso += tempo_porcentagem_progresso
+    // Atualiza visual da barra total
+    barra2.style.backgroundSize = `${soma_progresso + intense}% 100%`;
+    n_barra2.textContent = `${soma_progresso + intense}%`;
+    hr_span.textContent = "00"
+    min_span.textContent = "00"
+    seg_span.textContent = "00"
+
+    // Reseta barra1
+    tempo = 0;
+    barra1.style.backgroundSize = `0% 100%`;
+    n_barra1.textContent = "0%";
+    console.log("Progresso acumulado:", controle_barra2);
+}
