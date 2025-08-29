bed9e1914d97b2701e6c2a3d700a15aaed610da3
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-08-28 21:39:15 -0300

    Ajuste JS timer
    timer tá contando certo agora, barra vai para a direita como deve e progresso salva com intensidade. sem duplicação.

--- script.js
+++ script.js
@@ -28,52 +28,44 @@ function iniciar_timer() {
         }
         barra1.style.backgroundSize = `${tempo_porcentagem}%, 100%`;
         n_barra1.textContent = tempo_porcentagem + "%";
-        if (seg <= 58) {
+        if (seg < 59) {
             seg++;
-            seg_span.textContent = seg < 9 ? `0${seg}` : seg;
-        } else if (min <= 58) {
-            if (min < 9) {
-                min++;
-                min_span.textContent = `0${min}`;
-                seg_span.textContent = `00`;
-            } else {
-                min++;
-                min_span.textContent = min;
-            }
-            seg = 0;
+            seg_span.textContent = seg < 10 ? `0${seg}` : seg;
         } else {
-            hr++
-            hr_span.textContent = hr
-            min = 0;
             seg = 0;
             seg_span.textContent = "00";
-            min_span.textContent = "00";
+            if (min < 59) {
+                min++;
+                min_span.textContent = min < 10 ? `0${min}` : min;
+            } else {
+                min = 0;
+                min_span.textContent = "00";
+                hr++;
+                hr_span.textContent = hr;
+            }
         }
-
-    }, 1000);
+    }, 1);
 }
 
 function parar_timer() {
     clearInterval(intervalo);
 }
 
-let controle_barra2 = 0
+let soma_progresso = 0
 function salvar() {
-    let intensidade = document.querySelector('#intensidade_id');
-    let intense = Number(intensidade.value);
-    let tempo_dividido = parseInt(tempo / 5);
-
-    // Atualiza controle_barra2 acumulando progresso
-    controle_barra2 += tempo_dividido + intense;
-
+    hr = 0, min = 0, seg = 0;
+    let tempo_porcentagem_progresso = parseInt((tempo / 18000) * 100);
+    soma_progresso += tempo_porcentagem_progresso
     // Atualiza visual da barra total
-    barra2.style.backgroundSize = `${controle_barra2}% 100%`;
-    n_barra2.textContent = `${controle_barra2}%`;
+    barra2.style.backgroundSize = `${soma_progresso + intense}% 100%`;
+    n_barra2.textContent = `${soma_progresso + intense}%`;
+    hr_span.textContent = "00"
+    min_span.textContent = "00"
+    seg_span.textContent = "00"
 
     // Reseta barra1
     tempo = 0;
     barra1.style.backgroundSize = `0% 100%`;
     n_barra1.textContent = "0%";
-
     console.log("Progresso acumulado:", controle_barra2);
 }
